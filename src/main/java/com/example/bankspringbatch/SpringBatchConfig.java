e2dbf86181ea3a28fd3e7a39049383878e8feca2
abed7helmi <abed.helmi@yahoo.fr>
2025-07-24 23:31:24 +0200

    ajout classe config et data

---
+++ src/main/java/com/example/bankspringbatch/SpringBatchConfig.java
@@ -0,0 +1,10 @@
+package com.example.bankspringbatch;
+
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing // pour activer springBatch
+public class SpringBatchConfig {
+}
