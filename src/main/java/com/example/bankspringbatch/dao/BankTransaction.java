750b7b28803b23e3180b61fd6feecada4b29a562
abed7helmi <abed.helmi@yahoo.fr>
2025-07-24 23:39:16 +0200

    ajout entity et repo

---
+++ src/main/java/com/example/bankspringbatch/dao/BankTransaction.java
@@ -0,0 +1,29 @@
+package com.example.bankspringbatch.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import java.util.Date;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class BankTransaction {
+    @Id
+    private Long id;
+    private long accountID;
+    private Date transactionDate ;
+    @Transient
+    private String strTransactionDate;
+    private String TransactionType;
+    private double amount;
+
+}
